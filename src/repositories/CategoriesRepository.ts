01e79e2f3e9aab8692c88c16fa9879616ea7eaa3
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-09-04 10:17:42 -0300

    validacao post

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+import Category from "../models/Category";
+import { ICreateCategoryDTO } from "../Interfaces/Category/ICreateCategory";
+
+class CategoriesRepository{
+    private categories: Category[];
+
+    constructor(){
+        this.categories=[]
+    }
+
+    create({name, description}:ICreateCategoryDTO){
+        const category= new Category(); 
+        Object.assign(category, {
+            name,
+            description,
+            created_at:new Date()
+        })
+
+        this.categories.push(category);
+    }
+
+    read():Category[] {
+        return this.categories;
+    }
+
+    findByName(name: string):Category{
+        const category=this.categories.find((category)=>category.name === name);
+        return category;
+    }
+
+}
+
+export default CategoriesRepository;
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,25 @@
 import { request, response, Router } from "express";
-import { v4 as uuidv4 } from "uuid";
-import Category from "../models/Category";
+import CategoriesRepository from "../repositories/CategoriesRepository";
+
 
 const categoriesRoutes = Router();
-const categories : Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response)=>{
     const {name, description} = request.body;
-    const category= new Category(); 
-    Object.assign(category, {
-        name,
-        description,
-        created_at:new Date()
-    })
-
-
-    categories.push(category);
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+    if(categoryAlreadyExists){
+        return response.status(400).json({error:"category already exists!"});
+    }
+    categoriesRepository.create({name, description})
 
     return response.status(201).send();
 });
+
+categoriesRoutes.get("/", (request, response)=>{
+    const all = categoriesRepository.read();
+
+    return response.json(all);
+})
 
 export default categoriesRoutes;
