994fb5b55cf25183dff54fab48b62732d460561d
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-09-06 10:46:27 -0300

    start swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { router } from "./routes";
+import SwaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
+
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerFile))
 app.use(router)
 
 
