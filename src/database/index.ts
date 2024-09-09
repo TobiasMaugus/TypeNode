83313d8b2a84bdb1050c8853c42c86b1170a1d6c
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-09-09 15:11:27 -0300

    container db

---
+++ src/database/index.ts
@@ -0,0 +1,28 @@
+import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+
+// Carrega variáveis de ambiente do arquivo .env, caso esteja rodando localmente
+dotenv.config();
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST || "localhost", // usa "localhost" por padrão (para rodar localmente)
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME || "docker",
+    password: process.env.DB_PASSWORD || "admin",
+    database: process.env.DB_DATABASE || "rentx",
+    entities: [],
+    synchronize: true,
+    logging: false,
+});
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+    });
+
+    
+export default AppDataSource;
