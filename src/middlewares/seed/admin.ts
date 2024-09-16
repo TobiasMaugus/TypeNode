fce06a86d2151c72f5b22bdbb84bf0bb6156981a
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-09-16 15:38:03 -0300

    seed UserIsAdmin

---
+++ src/middlewares/seed/admin.ts
@@ -0,0 +1,18 @@
+import AppDataSource from "../../database";
+import { v4 } from "uuid";
+import argon2 from "argon2";
+
+async function create() {
+    const connection = await AppDataSource.initialize();
+
+    const id = v4();
+    const password = await argon2.hash("admin");
+
+    await connection.query(
+        `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+        values('${id}', 'admin', 'admin@admin.com', '${password}', true, 'now()', '123123'`
+    )
+    await connection.destroy();
+}
+
+create().then(() => console.log("user admin created")).catch(error => console.log(error));
