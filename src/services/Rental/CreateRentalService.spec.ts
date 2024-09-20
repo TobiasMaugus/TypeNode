f7f84560def132e2cc27199c2134d86ffcb766e8
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-09-20 10:17:27 -0300

    rental test ok

--- src/services/Rental/CreateRentalService.spec.ts
+++ src/services/Rental/CreateRentalService.spec.ts
@@ -1,4 +1,5 @@
 import RentalsRepositoryInMemory from "../../repositories/in-memory/RentalsRepositoryIM";
+import AppError from "../../shared/errors/AppError";
 import CreateRentalService from "./CreateRentalService"
 
 let createRentalService:CreateRentalService;
@@ -11,10 +12,45 @@ describe("Create Rental", ()=>{
     });
 
     it("should be able to create a new rental", async ()=>{
-        await createRentalService.execute({
+        const rental = await createRentalService.execute({
             user_id:"12345",
             car_id:"121212",
             expected_return_date: new Date()
         })
+
+        expect(rental).toHaveProperty("id");
+        expect(rental).toHaveProperty("start_date");
+    })
+
+    it("should not be able to create a new rental if there is another open to the same user", async ()=>{
+        expect(async()=>{
+            await createRentalService.execute({
+                user_id:"12345",
+                car_id:"121211",
+                expected_return_date: new Date()
+            })
+            
+            const rental = await createRentalService.execute({
+                user_id:"12345",
+                car_id:"121212",
+                expected_return_date: new Date()
+            })
+        }).rejects.toBeInstanceOf(AppError)
+    })
+
+    it("should not be able to create a new rental if there is another open to the same user", async ()=>{
+        expect(async()=>{
+            await createRentalService.execute({
+                user_id:"123333",
+                car_id:"121212",
+                expected_return_date: new Date()
+            })
+            
+            const rental = await createRentalService.execute({
+                user_id:"12345",
+                car_id:"121212",
+                expected_return_date: new Date()
+            })
+        }).rejects.toBeInstanceOf(AppError)
     })
 })
