bd4b14884ef71020a8faec6696d7e43b132d899b
TobiasMaugus <tobiasmaugus705@gmail.com>
2024-09-23 16:44:14 -0300

    rental controller

--- src/controllers/Rental/CreateRentalController.ts
+++ src/controllers/Rental/CreateRentalController.ts
@@ -1,11 +1,19 @@
 import { container } from "tsyringe";
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import CreateRentalService from "../../services/Rental/CreateRentalService";
 
 class CreateRentalController{
     async handle(request:Request, response:Response):Promise<Response>{
-        return response;
-    }
+        const {car_id, expected_return_date, user_id} = request.body;
+        const createRentalService = container.resolve(CreateRentalService);
+        const rental = await createRentalService.execute({
+            car_id,
+            expected_return_date,
+            user_id
+        })
+            return response.status(201).json(rental);
+        }      
 }
+
 
 export default CreateRentalController
